547781f1d804be2bdb2e6c4b06d8a87407cc9d6c
Himanshu-007-tech <himanshuchakraborty05@gmail.com>
2025-12-10 18:10:22 +0530

    Add files via upload

---
+++ chapter 2.py/02datatyped.py
@@ -0,0 +1,5 @@
+a = 1 # a is an integer
+b = 5.22 # b is a floating point number 
+c = "Harry" # c is string
+d = False # d is boolean variable
+e = None # e is a none type variable
---
+++ chapter 2.py/04operator.py
@@ -0,0 +1,36 @@
+# Arithmetic operators
+a = 3 
+b = 4
+c = a+b 
+print(c)
+
+#Assignment Operators
+a = 4 - 3 # Assign 4-2 in a
+print (a)
+b = 6 
+#b +=3 # incremant the value of b by 3 and then assign it to b 
+b -=3 # decremant the value of b by 3 and then assign it to b 
+print (b)
+
+# Comparison Operators
+
+d = 5<4
+print(d)
+
+#logical operators
+
+e = True or false 
+# Truth table of 'or'
+print("True or False is", True or False)
+print("True or True is", True or False)
+print("False or False is", True or False)
+print("False or False is", True or False)
+
+# Truth table of 'and'
+print("True or False is", True or False)
+print("True or True is", True or False)
+print("False or False is", True or False)
+print("False or False is", True or False)
+
+print(not(True))
+
