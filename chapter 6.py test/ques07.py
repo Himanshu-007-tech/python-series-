d38e84bbf84ef381578389e5c422fa504b69d3d7
Himanshu-007-tech <himanshuchakraborty05@gmail.com>
2025-12-20 20:48:37 +0530

    Add files via upload

---
+++ chapter 6.py test/ques07.py
@@ -0,0 +1,9 @@
+post = input("Enter the post:")
+
+if("Himanshu".lower()in post.lower()):
+    print("This post is talking about himanshu")
+
+else:
+    print("This post is not talking about himanshu")
+
+#.lower() case talking about the implementation of post 
