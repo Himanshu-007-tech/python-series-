79727158aa25f50e2b8010dc1cec0922509c263d
Himanshu-007-tech <himanshuchakraborty05@gmail.com>
2026-01-26 20:16:52 +0530

    Add files via upload

---
+++ chapter 10.py/01_class.py
@@ -0,0 +1,14 @@
+class Employee:
+    language = "py"# This is s class atributes
+    salary = "12000"
+
+Himanshu = Employee()
+Himanshu.name = "Himanshu"# THIS is an instance attributes
+print(Himanshu.name, Himanshu.language, Himanshu.salary)
+
+Ravi = Employee()
+Ravi.name = "Ravi Roro Robinson"
+print(Ravi.name, Ravi.salary, Ravi.language)
+
+# Here name is object instances attributes and salary and language are class attributes ad they directly belong to the class
+
