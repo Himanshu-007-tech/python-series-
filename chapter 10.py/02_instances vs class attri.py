2cffd55d0fe0358b086bab850941506d4e9b5c6f
Himanshu-007-tech <himanshuchakraborty05@gmail.com>
2026-01-27 18:15:55 +0530

    Add files via upload

---
+++ chapter 10.py/02_instances vs class attri.py
@@ -0,0 +1,7 @@
+class Employee:
+    language = "Python" # This is a class attributes
+    salary = 130000
+
+himanshu = Employee()
+himanshu.language = "Javascript" # This is an instances attributes
+print(himanshu.language, himanshu.salary)
