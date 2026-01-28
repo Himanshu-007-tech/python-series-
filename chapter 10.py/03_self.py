8a0196dcf421d5545cd7b339f540bb8dd9b775a8
Himanshu-007-tech <himanshuchakraborty05@gmail.com>
2026-01-28 18:29:16 +0530

    Add files via upload

---
+++ chapter 10.py/03_self.py
@@ -0,0 +1,15 @@
+class Employee:
+    language = "Python" # This is a class attributes
+    salary = 130000
+
+    def getInfo():
+        print(f"The language is {self.language}. The salary is {self.salary}") 
+
+        @staticmethod
+    
+        def greet(self):
+         print("Good morning")
+himanshu = Employee()
+himanshu.greet()
+himanshu.greetInfo()
+
