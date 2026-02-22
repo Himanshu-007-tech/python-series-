9cf3f8891a20f003116ea6eec9cbb36f1cf9102d
Himanshu-007-tech <himanshuchakraborty05@gmail.com>
2026-02-22 09:24:13 +0530

    Add files via upload

---
+++ chapter 12.py/04 list_methods.py
@@ -0,0 +1,20 @@
+friends = ["Apple", "Orange", 5, 76, False, "Akash", "Rohan"]
+print(friends[0])
+friends[0] = "Grapes" # unlike strings the list are mutable
+print(friends[0])
+friends.append("himanshu")
+print(friends)
+l1 = [1,7,6,9,5,4]
+# l1.sort()
+print(l1)
+# l1.reverse()
+print(l1)
+
+# l1.insert(3, 333333) #insert 333333 such that its index in the lists is 3
+print(l1)
+
+# print(l1.pop(0))
+print(l1)
+
+l1.remove(5)
+print(l1)
---
+++ chapter 12.py/04_tuples_methods.py
@@ -0,0 +1,12 @@
+a = (1,34,67,78,False,"Ram","Shivam")
+print(a)
+no = a.count(34)
+print(no)
+i = a.index(3434)
+print(i)
+
+my_tuple = (1, 2, 3)
+
+repeated = my_tuple * 3
+print(repeated) 
+
